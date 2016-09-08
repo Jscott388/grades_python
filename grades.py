e086533aa876d156405bcb34f59a8c55e896fb54
Joshua <joshua@Joshuas-Mac-Pro.local>
2016-09-07 20:13:39 -0400

    init commit

---
+++ grades.py
@@ -0,0 +1,20 @@
+def grade():
+    for count in range(0,11):
+        data = raw_input("What is your score?")
+        score = int(data)
+        if 60 <= score <= 69:
+            print "Scores:", score , ": Your grade is D"
+        elif 70 <= score <= 79:
+            print "Scores:", score , ": Your grade is C"
+        elif 80 <= score <= 89:
+            print "Scores:", score , ": Your grade is B"
+        elif 90 <= score <= 100:
+            print "Scores:", score , ": Your grade is A"
+        elif score > 100:
+            print "You scored over 100! Great job"
+        else:
+            print "You have failed"
+
+    print "End of program, Bye!"
+
+grade()
